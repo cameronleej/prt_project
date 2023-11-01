244fa27a1aba83adea377220cd072b40348cd01a
Cameron Johnson <clj00004@mix.wvu.edu>
2023-11-01 14:20:07 -0400

    firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyDHIs6xtIZrJ8BypJ2hZJLJcyd0lvw1u6s",
+
+  authDomain: "prt-project-edc2c.firebaseapp.com",
+
+  projectId: "prt-project-edc2c",
+
+  storageBucket: "prt-project-edc2c.appspot.com",
+
+  messagingSenderId: "977381220415",
+
+  appId: "1:977381220415:web:5b84e1d8ad331d73ff188a",
+
+  measurementId: "G-0L4RBLH27X"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
