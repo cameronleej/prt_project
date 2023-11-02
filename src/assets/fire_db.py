8b768a28cf54eea8df85e9c3d9b775da8c140c26
cameronleej <clj00004@mix.wvu.edu>
2023-11-01 21:49:56 -0400

    added data push functionality testing

--- src/assets/fire_db.py
+++ src/assets/fire_db.py
@@ -1,22 +1,15 @@
-import datetime
 from csv import reader
-import csv
 import pyrebase
 
 firebaseConfig = {
 
   "apiKey": "AIzaSyDHIs6xtIZrJ8BypJ2hZJLJcyd0lvw1u6s",
-
   "authDomain": "prt-project-edc2c.firebaseapp.com",
-
+  "databaseURL": "https://prt-project-edc2c-default-rtdb.firebaseio.com/",
   "projectId": "prt-project-edc2c",
-
   "storageBucket": "prt-project-edc2c.appspot.com",
-
   "messagingSenderId": "977381220415",
-
   "appId": "1:977381220415:web:5b84e1d8ad331d73ff188a",
-
   "measurementId": "G-0L4RBLH27X"
 
 }
@@ -35,9 +28,10 @@ load_csv()
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
-storage = firebase.storage()
 database = firebase.database()
 
+##push data
+
 database.child("value")
-data = {"time": "01:00", "Current": "3.3333"}
-database.set(data)
+data = {"day": "11/1/2023", "time": "01:00", "Current": "3.3333"}
+database.push(data)
